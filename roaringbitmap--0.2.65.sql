ed9613fad0d53113132967fe1965da7e2168e7cd
zeromax007 <zeromax@live.cn>
2019-12-10 19:17:17 +0800

    init

--- roaringbitmap--0.2.65.sql
+++ roaringbitmap--0.2.65.sql
@@ -1,29 +1,19 @@
 --- Create by ZEROMAX on 2017/3/20 ---
-
-SET search_path = pg_catalog;
-
+SET
+  search_path = pg_catalog;
 --- data type --
-
-CREATE OR REPLACE FUNCTION roaringbitmap_in(cstring)
-   RETURNS roaringbitmap
-   AS 'MODULE_PATHNAME','roaringbitmap_in'
-   LANGUAGE C STRICT IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION roaringbitmap_out(roaringbitmap)
-   RETURNS cstring
-   AS 'MODULE_PATHNAME','roaringbitmap_out'
-   LANGUAGE C STRICT IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION roaringbitmap_recv(internal)
-   RETURNS roaringbitmap
-   AS 'MODULE_PATHNAME','roaringbitmap_recv'
-   LANGUAGE C STRICT IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION roaringbitmap_send(roaringbitmap)
-   RETURNS bytea
-   AS 'MODULE_PATHNAME','roaringbitmap_send'
-   LANGUAGE C STRICT IMMUTABLE;
-
+  CREATE
+  OR REPLACE FUNCTION roaringbitmap_in(cstring) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'roaringbitmap_in' LANGUAGE C STRICT IMMUTABLE;
+CREATE
+  OR REPLACE FUNCTION roaringbitmap_out(roaringbitmap) RETURNS cstring AS 'MODULE_PATHNAME',
+  'roaringbitmap_out' LANGUAGE C STRICT IMMUTABLE;
+CREATE
+  OR REPLACE FUNCTION roaringbitmap_recv(internal) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'roaringbitmap_recv' LANGUAGE C STRICT IMMUTABLE;
+CREATE
+  OR REPLACE FUNCTION roaringbitmap_send(roaringbitmap) RETURNS bytea AS 'MODULE_PATHNAME',
+  'roaringbitmap_send' LANGUAGE C STRICT IMMUTABLE;
 CREATE TYPE roaringbitmap (
     INTERNALLENGTH = VARIABLE,
     INPUT = roaringbitmap_in,
@@ -31,245 +21,161 @@ CREATE TYPE roaringbitmap (
     receive = roaringbitmap_recv,
     send = roaringbitmap_send,
     STORAGE = external
- );
-
+  );
 -- functions --
-
-CREATE OR REPLACE FUNCTION rb_build(integer[])
-   RETURNS roaringbitmap 
-   AS 'MODULE_PATHNAME', 'rb_build'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_or(roaringbitmap, roaringbitmap)
-   RETURNS roaringbitmap 
-   AS 'MODULE_PATHNAME', 'rb_or'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_or_cardinality(roaringbitmap, roaringbitmap)
-   RETURNS integer
-   AS 'MODULE_PATHNAME', 'rb_or_cardinality'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_and(roaringbitmap, roaringbitmap)
-   RETURNS roaringbitmap 
-   AS 'MODULE_PATHNAME', 'rb_and'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_and_cardinality(roaringbitmap, roaringbitmap)
-   RETURNS integer
-   AS 'MODULE_PATHNAME', 'rb_and_cardinality'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_xor(roaringbitmap, roaringbitmap)
-   RETURNS roaringbitmap 
-   AS 'MODULE_PATHNAME', 'rb_xor'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_xor_cardinality(roaringbitmap, roaringbitmap)
-   RETURNS integer
-   AS 'MODULE_PATHNAME', 'rb_xor_cardinality'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_andnot(roaringbitmap, roaringbitmap)
-   RETURNS roaringbitmap 
-   AS 'MODULE_PATHNAME', 'rb_andnot'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_andnot_cardinality(roaringbitmap, roaringbitmap)
-   RETURNS integer
-   AS 'MODULE_PATHNAME', 'rb_andnot_cardinality'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_cardinality(roaringbitmap)
-   RETURNS integer 
-   AS 'MODULE_PATHNAME', 'rb_cardinality'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_is_empty(roaringbitmap)
-  RETURNS bool
-  AS  'MODULE_PATHNAME', 'rb_is_empty'
-   LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION rb_equals(roaringbitmap, roaringbitmap)
-  RETURNS bool
-  AS  'MODULE_PATHNAME', 'rb_equals'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_not_equals(roaringbitmap, roaringbitmap)
-  RETURNS bool 
-  AS  $$ SELECT NOT rb_equals($1, $2) $$
-   LANGUAGE SQL STRICT;
-
-CREATE OR REPLACE FUNCTION rb_intersect(roaringbitmap, roaringbitmap)
-  RETURNS bool
-  AS  'MODULE_PATHNAME', 'rb_intersect'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_contains(roaringbitmap, integer)
-  RETURNS bool
-  AS  'MODULE_PATHNAME', 'rb_contains'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_contains(roaringbitmap, integer, integer)
-  RETURNS bool
-  AS  'MODULE_PATHNAME', 'rb_contains_range'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_contains(roaringbitmap, roaringbitmap)
-  RETURNS bool
-  AS  'MODULE_PATHNAME', 'rb_contains_bitmap'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_becontained(integer, roaringbitmap)
-  RETURNS bool
-  AS $$ SELECT rb_contains($2, $1) $$  
-   LANGUAGE SQL STRICT;
-
-CREATE OR REPLACE FUNCTION rb_becontained(roaringbitmap, roaringbitmap)
-  RETURNS bool
-  AS $$ SELECT rb_contains($2, $1) $$
-   LANGUAGE SQL STRICT;
-
-CREATE OR REPLACE FUNCTION rb_add(roaringbitmap, integer)
-   RETURNS roaringbitmap
-   AS 'MODULE_PATHNAME', 'rb_add'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_add_2(integer, roaringbitmap)
-   RETURNS roaringbitmap
-   AS $$ SELECT rb_add($2, $1) $$
-   LANGUAGE SQL STRICT;
-
-CREATE OR REPLACE FUNCTION rb_add(roaringbitmap, integer, integer)
-   RETURNS roaringbitmap
-   AS 'MODULE_PATHNAME', 'rb_add_range'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_remove(roaringbitmap, integer)
-   RETURNS roaringbitmap
-   AS 'MODULE_PATHNAME', 'rb_remove'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_remove(roaringbitmap, integer, integer)
-   RETURNS roaringbitmap
-   AS 'MODULE_PATHNAME', 'rb_remove_range'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_flip(roaringbitmap, integer)
-   RETURNS roaringbitmap
-   AS 'MODULE_PATHNAME', 'rb_flip'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_flip(roaringbitmap, integer, integer)
-   RETURNS roaringbitmap
-   AS 'MODULE_PATHNAME', 'rb_flip_range'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_minimum(roaringbitmap)
-   RETURNS integer
-   AS 'MODULE_PATHNAME', 'rb_minimum'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_maximum(roaringbitmap)
-   RETURNS integer
-   AS 'MODULE_PATHNAME', 'rb_maximum'
-   LANGUAGE C STRICT;
-
- CREATE OR REPLACE FUNCTION rb_rank(roaringbitmap, integer)
-   RETURNS integer
-   AS 'MODULE_PATHNAME', 'rb_rank'
-   LANGUAGE C STRICT;
-
- CREATE OR REPLACE FUNCTION rb_jaccard_index(roaringbitmap, roaringbitmap)
-   RETURNS float8 
-   AS 'MODULE_PATHNAME', 'rb_jaccard_index'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_to_array(roaringbitmap)
-   RETURNS integer[] 
-   AS 'MODULE_PATHNAME', 'rb_to_array'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_iterate(roaringbitmap)
-   RETURNS SETOF integer 
-   AS 'MODULE_PATHNAME', 'rb_iterate'
-   LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION rb_serialize(internal)
-     RETURNS roaringbitmap
-     AS 'MODULE_PATHNAME', 'rb_serialize'
-     LANGUAGE C IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION rb_cardinality_trans(internal)
-     RETURNS integer
-     AS 'MODULE_PATHNAME', 'rb_cardinality_trans'
-     LANGUAGE C IMMUTABLE;
-
-
-CREATE OR REPLACE FUNCTION rb_or_trans(internal, roaringbitmap)
-     RETURNS internal
-      AS 'MODULE_PATHNAME', 'rb_or_trans'
-     LANGUAGE C IMMUTABLE;
-
-
--- operator -- 
-
-CREATE OPERATOR & (
-  LEFTARG = roaringbitmap,
-  RIGHTARG = roaringbitmap,
-  PROCEDURE = rb_and
-);
-
+  CREATE
+  OR REPLACE FUNCTION rb_build(integer []) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_build' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_or(roaringbitmap, roaringbitmap) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_or' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_or_cardinality(roaringbitmap, roaringbitmap) RETURNS integer AS 'MODULE_PATHNAME',
+  'rb_or_cardinality' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_and(roaringbitmap, roaringbitmap) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_and' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_and_cardinality(roaringbitmap, roaringbitmap) RETURNS integer AS 'MODULE_PATHNAME',
+  'rb_and_cardinality' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_xor(roaringbitmap, roaringbitmap) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_xor' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_xor_cardinality(roaringbitmap, roaringbitmap) RETURNS integer AS 'MODULE_PATHNAME',
+  'rb_xor_cardinality' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_andnot(roaringbitmap, roaringbitmap) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_andnot' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_andnot_cardinality(roaringbitmap, roaringbitmap) RETURNS integer AS 'MODULE_PATHNAME',
+  'rb_andnot_cardinality' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_cardinality(roaringbitmap) RETURNS integer AS 'MODULE_PATHNAME',
+  'rb_cardinality' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_is_empty(roaringbitmap) RETURNS bool AS 'MODULE_PATHNAME',
+  'rb_is_empty' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_equals(roaringbitmap, roaringbitmap) RETURNS bool AS 'MODULE_PATHNAME',
+  'rb_equals' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_not_equals(roaringbitmap, roaringbitmap) RETURNS bool AS $$
+SELECT
+  NOT rb_equals($1, $2) $$ LANGUAGE SQL STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_intersect(roaringbitmap, roaringbitmap) RETURNS bool AS 'MODULE_PATHNAME',
+  'rb_intersect' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_contains(roaringbitmap, integer) RETURNS bool AS 'MODULE_PATHNAME',
+  'rb_contains' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_contains(roaringbitmap, integer, integer) RETURNS bool AS 'MODULE_PATHNAME',
+  'rb_contains_range' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_contains(roaringbitmap, roaringbitmap) RETURNS bool AS 'MODULE_PATHNAME',
+  'rb_contains_bitmap' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_becontained(integer, roaringbitmap) RETURNS bool AS $$
+SELECT
+  rb_contains($2, $1) $$ LANGUAGE SQL STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_becontained(roaringbitmap, roaringbitmap) RETURNS bool AS $$
+SELECT
+  rb_contains($2, $1) $$ LANGUAGE SQL STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_add(roaringbitmap, integer) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_add' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_add_2(integer, roaringbitmap) RETURNS roaringbitmap AS $$
+SELECT
+  rb_add($2, $1) $$ LANGUAGE SQL STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_add(roaringbitmap, integer, integer) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_add_range' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_remove(roaringbitmap, integer) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_remove' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_remove(roaringbitmap, integer, integer) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_remove_range' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_flip(roaringbitmap, integer) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_flip' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_flip(roaringbitmap, integer, integer) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_flip_range' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_minimum(roaringbitmap) RETURNS integer AS 'MODULE_PATHNAME',
+  'rb_minimum' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_maximum(roaringbitmap) RETURNS integer AS 'MODULE_PATHNAME',
+  'rb_maximum' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_rank(roaringbitmap, integer) RETURNS integer AS 'MODULE_PATHNAME',
+  'rb_rank' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_jaccard_index(roaringbitmap, roaringbitmap) RETURNS float8 AS 'MODULE_PATHNAME',
+  'rb_jaccard_index' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_to_array(roaringbitmap) RETURNS integer [] AS 'MODULE_PATHNAME',
+  'rb_to_array' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_iterate(roaringbitmap) RETURNS SETOF integer AS 'MODULE_PATHNAME',
+  'rb_iterate' LANGUAGE C STRICT;
+CREATE
+  OR REPLACE FUNCTION rb_final(internal) RETURNS roaringbitmap AS 'MODULE_PATHNAME',
+  'rb_serialize' LANGUAGE C IMMUTABLE;
+CREATE
+  OR REPLACE FUNCTION rb_serialize(internal) RETURNS bytea AS 'MODULE_PATHNAME',
+  'rb_serialize' LANGUAGE C IMMUTABLE;
+CREATE
+  OR REPLACE FUNCTION rb_deserialize(bytea, internal) RETURNS internal AS 'MODULE_PATHNAME',
+  'rb_deserialize' LANGUAGE C IMMUTABLE;
+CREATE
+  OR REPLACE FUNCTION rb_cardinality_trans(internal) RETURNS integer AS 'MODULE_PATHNAME',
+  'rb_cardinality_trans' LANGUAGE C IMMUTABLE;
+CREATE
+  OR REPLACE FUNCTION rb_or_trans(internal, roaringbitmap) RETURNS internal AS 'MODULE_PATHNAME',
+  'rb_or_trans' LANGUAGE C IMMUTABLE;
+-- operator --
+  CREATE OPERATOR & (
+    LEFTARG = roaringbitmap,
+    RIGHTARG = roaringbitmap,
+    PROCEDURE = rb_and
+  );
 CREATE OPERATOR | (
-  LEFTARG = roaringbitmap,
-  RIGHTARG = roaringbitmap,
-  PROCEDURE = rb_or
-);
-
+    LEFTARG = roaringbitmap,
+    RIGHTARG = roaringbitmap,
+    PROCEDURE = rb_or
+  );
 CREATE OPERATOR # (
   LEFTARG = roaringbitmap,
   RIGHTARG = roaringbitmap,
   PROCEDURE = rb_xor
 );
-
 CREATE OPERATOR ~ (
   LEFTARG = roaringbitmap,
   RIGHTARG = roaringbitmap,
   PROCEDURE = rb_andnot
 );
-
 CREATE OPERATOR + (
   LEFTARG = roaringbitmap,
   RIGHTARG = integer,
   PROCEDURE = rb_add,
   COMMUTATOR = '+'
 );
-
-
 CREATE OPERATOR + (
   LEFTARG = integer,
   RIGHTARG = roaringbitmap,
   PROCEDURE = rb_add_2,
   COMMUTATOR = '+'
 );
-
-
 CREATE OPERATOR - (
   LEFTARG = roaringbitmap,
   RIGHTARG = integer,
   PROCEDURE = rb_remove
 );
-
 CREATE OPERATOR = (
   LEFTARG = roaringbitmap,
   RIGHTARG = roaringbitmap,
@@ -279,7 +185,6 @@ CREATE OPERATOR = (
   RESTRICT = eqsel,
   JOIN = eqjoinsel
 );
-
 CREATE OPERATOR <> (
   LEFTARG = roaringbitmap,
   RIGHTARG = roaringbitmap,
@@ -289,8 +194,7 @@ CREATE OPERATOR <> (
   RESTRICT = neqsel,
   JOIN = neqjoinsel
 );
-
-CREATE OPERATOR && (
+CREATE OPERATOR & & (
   LEFTARG = roaringbitmap,
   RIGHTARG = roaringbitmap,
   PROCEDURE = rb_intersect,
@@ -298,8 +202,7 @@ CREATE OPERATOR && (
   RESTRICT = contsel,
   JOIN = contjoinsel
 );
-
-CREATE OPERATOR @> (
+CREATE OPERATOR @ > (
   LEFTARG = roaringbitmap,
   RIGHTARG = integer,
   PROCEDURE = rb_contains,
@@ -307,8 +210,7 @@ CREATE OPERATOR @> (
   RESTRICT = contsel,
   JOIN = contjoinsel
 );
-
-CREATE OPERATOR @> (
+CREATE OPERATOR @ > (
   LEFTARG = roaringbitmap,
   RIGHTARG = roaringbitmap,
   PROCEDURE = rb_contains,
@@ -316,8 +218,7 @@ CREATE OPERATOR @> (
   RESTRICT = contsel,
   JOIN = contjoinsel
 );
-
-CREATE OPERATOR <@ (
+CREATE OPERATOR < @ (
   LEFTARG = integer,
   RIGHTARG = roaringbitmap,
   PROCEDURE = rb_becontained,
@@ -325,8 +226,7 @@ CREATE OPERATOR <@ (
   RESTRICT = contsel,
   JOIN = contjoinsel
 );
-
-CREATE OPERATOR <@ (
+CREATE OPERATOR < @ (
   LEFTARG = roaringbitmap,
   RIGHTARG = roaringbitmap,
   PROCEDURE = rb_becontained,
@@ -334,65 +234,48 @@ CREATE OPERATOR <@ (
   RESTRICT = contsel,
   JOIN = contjoinsel
 );
-
 -- aggragations --
-
 CREATE AGGREGATE rb_or_agg(roaringbitmap)(
-       SFUNC = rb_or_trans,
-       STYPE = internal,
-       FINALFUNC = rb_serialize
+  SFUNC = rb_or_trans,
+  STYPE = internal,
+  FINALFUNC = rb_serialize
 );
-
 CREATE AGGREGATE rb_or_cardinality_agg(roaringbitmap)(
-       SFUNC = rb_or_trans,
-       STYPE = internal,
-       FINALFUNC = rb_cardinality_trans
+  SFUNC = rb_or_trans,
+  STYPE = internal,
+  FINALFUNC = rb_cardinality_trans
 );
-
-CREATE OR REPLACE FUNCTION rb_and_trans(internal, roaringbitmap)
-     RETURNS internal
-      AS 'MODULE_PATHNAME', 'rb_and_trans'
-     LANGUAGE C IMMUTABLE;
-
+CREATE
+OR REPLACE FUNCTION rb_and_trans(internal, roaringbitmap) RETURNS internal AS 'MODULE_PATHNAME',
+'rb_and_trans' LANGUAGE C IMMUTABLE;
 CREATE AGGREGATE rb_and_agg(roaringbitmap)(
-       SFUNC = rb_and_trans,
-       STYPE = internal,
-       FINALFUNC = rb_serialize
+  SFUNC = rb_and_trans,
+  STYPE = internal,
+  FINALFUNC = rb_serialize
 );
-
-
 CREATE AGGREGATE rb_and_cardinality_agg(roaringbitmap)(
-       SFUNC = rb_and_trans,
-       STYPE = internal,
-       FINALFUNC = rb_cardinality_trans
+  SFUNC = rb_and_trans,
+  STYPE = internal,
+  FINALFUNC = rb_cardinality_trans
 );
-
-
-CREATE OR REPLACE FUNCTION rb_xor_trans(internal, roaringbitmap)
-     RETURNS internal
-      AS 'MODULE_PATHNAME', 'rb_xor_trans'
-     LANGUAGE C IMMUTABLE;
-
+CREATE
+OR REPLACE FUNCTION rb_xor_trans(internal, roaringbitmap) RETURNS internal AS 'MODULE_PATHNAME',
+'rb_xor_trans' LANGUAGE C IMMUTABLE;
 CREATE AGGREGATE rb_xor_agg(roaringbitmap)(
-       SFUNC = rb_xor_trans,
-       STYPE = internal,
-       FINALFUNC = rb_serialize
+  SFUNC = rb_xor_trans,
+  STYPE = internal,
+  FINALFUNC = rb_serialize
 );
-
-
 CREATE AGGREGATE rb_xor_cardinality_agg(roaringbitmap)(
-       SFUNC = rb_xor_trans,
-       STYPE = internal,
-       FINALFUNC = rb_cardinality_trans
+  SFUNC = rb_xor_trans,
+  STYPE = internal,
+  FINALFUNC = rb_cardinality_trans
 );
-
-CREATE OR REPLACE FUNCTION rb_build_trans(internal, integer)
-     RETURNS internal
-      AS 'MODULE_PATHNAME', 'rb_build_trans'
-     LANGUAGE C;
-
-CREATE  AGGREGATE rb_build_agg(integer)(
-       SFUNC = rb_build_trans,
-       STYPE = internal,
-       FINALFUNC = rb_serialize
+CREATE
+OR REPLACE FUNCTION rb_build_trans(internal, integer) RETURNS internal AS 'MODULE_PATHNAME',
+'rb_build_trans' LANGUAGE C;
+CREATE AGGREGATE rb_build_agg(integer)(
+  SFUNC = rb_build_trans,
+  STYPE = internal,
+  FINALFUNC = rb_serialize
 );
--- roaringbitmap.c
+++ roaringbitmap.c
@@ -717,6 +717,54 @@ Datum
     PG_RETURN_POINTER(r1);
 }
 
+//bitmap or combine
+PG_FUNCTION_INFO_V1(rb_or_combine);
+Datum rb_or_combine(PG_FUNCTION_ARGS);
+
+Datum
+    rb_or_combine(PG_FUNCTION_ARGS)
+{
+    MemoryContext aggctx;
+    MemoryContext oldcontext;
+    roaring_bitmap_t *r1;
+    roaring_bitmap_t *r2;
+
+    // We must be called as a transition routine or we fail.
+    if (!AggCheckCallContext(fcinfo, &aggctx))
+        ereport(ERROR,
+                (errcode(ERRCODE_DATA_EXCEPTION),
+                 errmsg("rb_or_combine outside transition context")));
+
+    if (PG_ARGISNULL(1))
+    {
+        if (PG_ARGISNULL(0))
+        {
+            PG_RETURN_NULL();
+        }
+        r1 = (roaring_bitmap_t *)PG_GETARG_POINTER(0);
+    }
+    else
+    {
+        oldcontext = MemoryContextSwitchTo(aggctx);
+
+        r2 = (roaring_bitmap_t *)PG_GETARG_POINTER(1);
+
+        if (PG_ARGISNULL(0))
+        {
+            r1 = roaring_bitmap_copy(r2);
+        }
+        else
+        {
+            r1 = (roaring_bitmap_t *)PG_GETARG_POINTER(0);
+            roaring_bitmap_or_inplace(r1, r2);
+        }
+
+        MemoryContextSwitchTo(oldcontext);
+    }
+
+    PG_RETURN_POINTER(r1);
+}
+
 //bitmap and trans
 PG_FUNCTION_INFO_V1(rb_and_trans);
 Datum rb_and_trans(PG_FUNCTION_ARGS);
@@ -761,6 +809,54 @@ Datum
             roaring_bitmap_and_inplace(r1, r2);
         }
         roaring_bitmap_free(r2);
+    }
+
+    PG_RETURN_POINTER(r1);
+}
+
+//bitmap and combine
+PG_FUNCTION_INFO_V1(rb_and_combine);
+Datum rb_and_combine(PG_FUNCTION_ARGS);
+
+Datum
+    rb_and_combine(PG_FUNCTION_ARGS)
+{
+    MemoryContext aggctx;
+    MemoryContext oldcontext;
+    roaring_bitmap_t *r1;
+    roaring_bitmap_t *r2;
+
+    // We must be called as a transition routine or we fail.
+    if (!AggCheckCallContext(fcinfo, &aggctx))
+        ereport(ERROR,
+                (errcode(ERRCODE_DATA_EXCEPTION),
+                 errmsg("rb_and_combine outside transition context")));
+
+    if (PG_ARGISNULL(1))
+    {
+        if (PG_ARGISNULL(0))
+        {
+            PG_RETURN_NULL();
+        }
+        r1 = (roaring_bitmap_t *)PG_GETARG_POINTER(0);
+    }
+    else
+    {
+        oldcontext = MemoryContextSwitchTo(aggctx);
+
+        r2 = (roaring_bitmap_t *)PG_GETARG_POINTER(1);
+
+        if (PG_ARGISNULL(0))
+        {
+            r1 = roaring_bitmap_copy(r2);
+        }
+        else
+        {
+            r1 = (roaring_bitmap_t *)PG_GETARG_POINTER(0);
+            roaring_bitmap_and_inplace(r1, r2);
+        }
+
+        MemoryContextSwitchTo(oldcontext);
     }
 
     PG_RETURN_POINTER(r1);
@@ -815,6 +911,54 @@ Datum
     PG_RETURN_POINTER(r1);
 }
 
+//bitmap xor combine
+PG_FUNCTION_INFO_V1(rb_xor_combine);
+Datum rb_xor_combine(PG_FUNCTION_ARGS);
+
+Datum
+    rb_xor_combine(PG_FUNCTION_ARGS)
+{
+    MemoryContext aggctx;
+    MemoryContext oldcontext;
+    roaring_bitmap_t *r1;
+    roaring_bitmap_t *r2;
+
+    // We must be called as a transition routine or we fail.
+    if (!AggCheckCallContext(fcinfo, &aggctx))
+        ereport(ERROR,
+                (errcode(ERRCODE_DATA_EXCEPTION),
+                 errmsg("rb_xor_combine outside transition context")));
+
+    if (PG_ARGISNULL(1))
+    {
+        if (PG_ARGISNULL(0))
+        {
+            PG_RETURN_NULL();
+        }
+        r1 = (roaring_bitmap_t *)PG_GETARG_POINTER(0);
+    }
+    else
+    {
+        oldcontext = MemoryContextSwitchTo(aggctx);
+
+        r2 = (roaring_bitmap_t *)PG_GETARG_POINTER(1);
+
+        if (PG_ARGISNULL(0))
+        {
+            r1 = roaring_bitmap_copy(r2);
+        }
+        else
+        {
+            r1 = (roaring_bitmap_t *)PG_GETARG_POINTER(0);
+            roaring_bitmap_xor_inplace(r1, r2);
+        }
+
+        MemoryContextSwitchTo(oldcontext);
+    }
+
+    PG_RETURN_POINTER(r1);
+}
+
 //bitmap build trans
 PG_FUNCTION_INFO_V1(rb_build_trans);
 Datum rb_build_trans(PG_FUNCTION_ARGS);
@@ -861,7 +1005,6 @@ Datum
     rb_serialize(PG_FUNCTION_ARGS)
 {
     MemoryContext aggctx;
-
     roaring_bitmap_t *r1;
 
     // We must be called as a transition routine or we fail.
@@ -889,12 +1032,46 @@ Datum
     }
 }
 
-//bitmap Cardinality trans
-PG_FUNCTION_INFO_V1(rb_cardinality_trans);
-Datum rb_cardinality_trans(PG_FUNCTION_ARGS);
+//bitmap Deserialize
+PG_FUNCTION_INFO_V1(rb_deserialize);
+Datum rb_deserialize(PG_FUNCTION_ARGS);
 
 Datum
-    rb_cardinality_trans(PG_FUNCTION_ARGS)
+    rb_deserialize(PG_FUNCTION_ARGS)
+{
+    MemoryContext aggctx;
+    roaring_bitmap_t *r1;
+
+    // We must be called as a transition routine or we fail.
+    if (!AggCheckCallContext(fcinfo, &aggctx))
+        ereport(ERROR,
+                (errcode(ERRCODE_DATA_EXCEPTION),
+                 errmsg("rb_deserialize outside aggregate context")));
+
+    // Is the first argument a NULL?
+    if (PG_ARGISNULL(0))
+    {
+        PG_RETURN_NULL();
+    }
+    else
+    {
+        bytea *serializedbytes = PG_GETARG_BYTEA_P(0);
+        r1 = roaring_bitmap_portable_deserialize(VARDATA(serializedbytes));
+        if (!r1)
+            ereport(ERROR,
+                    (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED),
+                     errmsg("bitmap format is error")));
+
+        PG_RETURN_POINTER(r1);
+    }
+}
+
+//bitmap Cardinality Final
+PG_FUNCTION_INFO_V1(rb_cardinality_final);
+Datum rb_cardinality_final(PG_FUNCTION_ARGS);
+
+Datum
+    rb_cardinality_final(PG_FUNCTION_ARGS)
 {
     MemoryContext aggctx;
 
@@ -904,7 +1081,7 @@ Datum
     if (!AggCheckCallContext(fcinfo, &aggctx))
         ereport(ERROR,
                 (errcode(ERRCODE_DATA_EXCEPTION),
-                 errmsg("rb_cardinality_trans outside aggregate context")));
+                 errmsg("rb_cardinality_final outside aggregate context")));
 
     // Is the first argument a NULL?
     if (PG_ARGISNULL(0))
